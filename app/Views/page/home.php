b8e2f593a5c73311572871ea95490b014757034a
Julius-745 <juliusagung123@gmail.com>
2021-02-20 23:45:59 +0700

    make sure design and fix link -ulasan

--- app/Views/page/home.php
+++ app/Views/page/home.php
@@ -12,10 +12,10 @@
       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <div class="jumbotron bg-1" style="background-image: url('/artboard4.jpg'); background-position: center;"></div>
+            <div class="jumbotron bg-1" style="background-image: url('/artboard4.png'); background-position: center;"></div>
           </div>
           <div class="carousel-item">
-            <div class="jumbotron" style="background: url('/artboard2.jpg'); background-position: center;"></div>
+            <div class="jumbotron bg-2" style="background: url('/artboard2.jpg'); background-position: center;"></div>
           </div>
           <div class="carousel-item">
             <div class="jumbotron" style="background: url('/artboard3.jpg'); background-position: center;"></div>
@@ -30,7 +30,7 @@
       <div class="container">
         <h2 class="text-center">Layanan Kami</h2><br />
         <div class="row">
-          <div class="col-4">
+          <div class="col-3">
             <a href="/toko/">
               <div class="card text-center">
                 <div class="card-body">
@@ -40,12 +40,22 @@
               </div>
             </a>
           </div>
-          <div class="col-4">
+          <div class="col-3">
             <a href="/custom/" data-toggle="modal" data-target="#sendModal">
               <div class="card text-center">
                 <div class="card-body">
                   <img src="/food-delivery.png" width="100" alt="">
-                  <h5 class="card-title">Custom Order</h5>
+                  <h5 class="card-title">Pesan Di Tempat Berbeda</h5>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-3">
+            <a href="/custom/" data-toggle="modal" data-target="#sendModal">
+              <div class="card text-center">
+                <div class="card-body">
+                  <img src="/food-delivery.png" width="100" alt="">
+                  <h5 class="card-title">Antar Jemput</h5>
                 </div>
               </div>
             </a>
@@ -58,9 +68,9 @@
 
   <div class="not-list-section">
     <div class="container">
-      <h3 class="text-center">Pengen beli makanan favorit kamu dari rumah ?<br>Langsung daftarkan diri anda</h3>
+      <h3 class="text-center">Bantu kami untuk mengetahui cara mengoptimalkan kinerja kami<br>Ulas kami dengan</h3>
       <p class="text-center mt-5">
-        <a href="/toko/" class="btn btn-order">Klik Disini</a>
+        <a href="/custom/" class="btn btn-order">Klik Disini</a>
       </p>
     </div>
   </div>
