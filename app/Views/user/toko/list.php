d3012f89c482229e916cee4a86927f68ff8bca5f
Julius-745 <juliusagung123@gmail.com>
2021-02-22 17:27:14 +0700

    wes

--- app/Views/user/toko/list.php
+++ app/Views/user/toko/list.php
@@ -9,22 +9,14 @@
     <?php /** @var \App\Entities\Toko[] $data */ ?>
     <h2 class="mb-3">Belanja Aneka Toko Disini:</h2>
     <div class="row user-choose">
-        <div class="col-lg-10 col-md-8">
-          <div class="card-body">
-              <?php foreach ($data as $toko) : ?>
-                <div class="col-md-6 col-lg-4 col-xl-3 col-6 user-item">
-                <a class="item" style="width: 20%;" href="/toko/view/<?= $toko->id ?>">
-                  <img src="/uploads/logo/<?= $toko->logo ?>" alt="" width="100%">
-                  <h4><?= $toko->nama ?></h4>
-                </a>
-                <a class="item" style="width: 20%;" href="/toko/view/<?= $toko->id ?>">
-                  <img src="/uploads/logo/<?= $toko->logo ?>" alt="" width="100%">
-                  <h4><?= $toko->nama ?></h4>
-                </a>
-              <?php endforeach ?>
-            </div>
-          </div>
+      <?php foreach ($data as $toko) : ?>
+        <div class="col-md-6 col-lg-4 col-xl-3 col-6 user-item">
+          <a class="item" style="width: 20%;" href="/toko/view/<?= $toko->id ?>">
+            <img src="/uploads/logo/<?= $toko->logo ?>" alt="" width="100%">
+            <h4><?= $toko->nama ?></h4>
+          </a>
         </div>
+      <?php endforeach ?>
     </div>
   </div>
 
