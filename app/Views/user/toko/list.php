6687dabc946600766a64b2eba5447b4fb6bb0ae1
Julius-745 <juliusagung123@gmail.com>
2021-02-22 17:43:48 +0700

    kurang teliti

--- app/Views/user/toko/list.php
+++ app/Views/user/toko/list.php
@@ -11,7 +11,7 @@
     <div class="row user-choose">
       <?php foreach ($data as $toko) : ?>
         <div class="col-md-6 col-lg-4 col-xl-3 col-6 user-item">
-          <a class="item" style="width: 20%;" href="/toko/view/<?= $toko->id ?>">
+          <a class="item" href="/toko/view/<?= $toko->id ?>">
             <img src="/uploads/logo/<?= $toko->logo ?>" alt="" width="100%">
             <h4><?= $toko->nama ?></h4>
           </a>
