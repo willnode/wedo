2fee200ef3ea2cad8fb570c4e01e8b3720f24a5a
Julius-745 <juliusagung123@gmail.com>
2021-02-22 15:44:39 +0700

    list done script terguak

--- app/Views/user/toko/list.php
+++ app/Views/user/toko/list.php
@@ -9,65 +9,26 @@
     <?php /** @var \App\Entities\Toko[] $data */ ?>
     <h2 class="mb-3">Belanja Aneka Toko Disini:</h2>
     <div class="row user-choose">
-      <?php foreach ($data as $toko) : ?>
-        <div class="col-lg-2 col-md-4">
-          <a class="item" href="/toko/view/<?= $toko->id ?>">
-            <img src="/uploads/logo/<?= $toko->logo ?>" alt="" width="100px">
-            <h4><?= $toko->nama ?></h4>
-          </a>
-        </div>
         <div class="col-lg-10 col-md-8">
           <div class="card">
             <div class="slick card-body invisible">
-              <?php foreach ($toko->getBarang() as $barang) : ?>
-                <a class="item" style="width: 20%;" href="/barang/view/<?= $barang->id ?>">
-                  <img src="/uploads/logo/<?= $barang->logo ?>" alt="" width="100%">
-                  <h4><?= $barang->nama ?></h4>
-                  <div class="text-center text-black-50"><?= rupiah($barang->harga) ?></div>
+              <?php foreach ($data as $toko) : ?>
+                <a class="item" style="width: 20%;" href="/toko/view/<?= $toko->id ?>">
+                  <img src="/uploads/logo/<?= $toko->logo ?>" alt="" width="100%">
+                  <h4><?= $toko->nama ?></h4>
+                </a>
+                <a class="item" style="width: 20%;" href="/toko/view/<?= $toko->id ?>">
+                  <img src="/uploads/logo/<?= $toko->logo ?>" alt="" width="100%">
+                  <h4><?= $toko->nama ?></h4>
                 </a>
               <?php endforeach ?>
             </div>
           </div>
         </div>
-      <?php endforeach ?>
     </div>
   </div>
 
   <?= view('shared/footer.php'); ?>
-  <script>
-    window.addEventListener('DOMContentLoaded', (event) => {
-      $('.slick').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        arrows: true,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      })
-      $('.slick').removeClass('invisible');
-    });
-  </script>
 </body>
 
 </html>
